8556294bfd02d8fadc99b14cbeba6f8a709b6d71
Darkkid819 <darkkid8192004@gmail.com>
2024-08-07 01:20:30 -0400

    added SDL keyboard input

--- chip8.c
+++ chip8.c
@@ -58,15 +58,48 @@ void renderDisplay(void) {
     SDL_RenderPresent(renderer);
 }
 
+uint8_t sdlKeyToChip8Key(SDL_Keycode key) {
+    switch (key) {
+        case SDLK_1: return 0x1;
+        case SDLK_2: return 0x2;
+        case SDLK_3: return 0x3;
+        case SDLK_4: return 0xC;
+        case SDLK_q: return 0x4;
+        case SDLK_w: return 0x5;
+        case SDLK_e: return 0x6;
+        case SDLK_r: return 0xD;
+        case SDLK_a: return 0x7;
+        case SDLK_s: return 0x8;
+        case SDLK_d: return 0x9;
+        case SDLK_f: return 0xE;
+        case SDLK_z: return 0xA;
+        case SDLK_x: return 0x0;
+        case SDLK_c: return 0xB;
+        case SDLK_v: return 0xF;
+        default: return 0xFF;  // invalid key
+    }
+}
+
 int handleEvents(void) {
     SDL_Event e;
     while (SDL_PollEvent(&e) != 0) {
         if (e.type == SDL_QUIT) {
             return 0;
+        } else if (e.type == SDL_KEYDOWN) {
+            uint8_t chip8Key = sdlKeyToChip8Key(e.key.keysym.sym);
+            if (chip8Key != 0xFF) {
+                setKeyDown(chip8Key);
+            }
+        } else if (e.type == SDL_KEYUP) {
+            uint8_t chip8Key = sdlKeyToChip8Key(e.key.keysym.sym);
+            if (chip8Key != 0xFF) {
+                setKeyUp(chip8Key);
+            }
         }
     }
     return 1;
 }
+
 
 void cleanupSDL(void) {
     SDL_DestroyTexture(texture);
